4b6d246af4a490c5da09886e7c9f8e71a1bcc26b
Philipp Rollinger <philipp.rollinger@gmail.com>
2025-05-21 18:40:47 +0700

    Prepare an example project

--- djinx/setup.py
+++ djinx/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-django-package',
+    name='djinx',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['django>=3.2'],
+    python_requires='>=3.10,<3.11',
+    install_requires=[
+        'Django>=4.2,<4.3',  # Django 4.2 is the latest LTS
+    ],
     classifiers=[
         'Framework :: Django',
         'Programming Language :: Python :: 3',
