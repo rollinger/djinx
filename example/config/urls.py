05148e0e6a7bb380a3bc20592586c9b4b819d86b
Philipp Rollinger <philipp.rollinger@gmail.com>
2026-06-23 13:39:09 +0700

    first prototype

--- example/config/urls.py
+++ example/config/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
+
+from .views.djinx import MyDjinxBattery
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("dxe/", include(MyDjinxBattery.router())),
 ]
