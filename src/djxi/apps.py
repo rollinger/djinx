d81db1ae0c889c861b26dd1e425053b6c676e304
Philipp Rollinger <philipp.rollinger@gmail.com>
2026-06-27 11:27:21 +0700

    test app.ready conf validation

--- src/djxi/apps.py
+++ src/djxi/apps.py
@@ -12,7 +12,7 @@ class DjxiAppConfig(AppConfig):
         This is the safest place to import signals, validators, or run
         one-time startup code.
         """
-        # Validate required settings exist and sane
+        # Validate required settings exist and are sane
         from .conf import package_settings
 
         htmx_version = package_settings.DX_HTMX_VERSION
@@ -20,7 +20,7 @@ class DjxiAppConfig(AppConfig):
             raise ImproperlyConfigured("DX_HTMX_VERSION must be 2 or 4")
 
         section_tag = package_settings.DX_SECTION_TAG
-        if section_tag in ["", None]:
+        if not isinstance(section_tag, str) or section_tag == "":
             raise ImproperlyConfigured("DX_SECTION_TAG can not be None or empty")
 
         # Optional: Import signals if you have a signals.py file
--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -1,5 +1,9 @@
+import pytest
+import types
+from django.core.exceptions import ImproperlyConfigured
 from django.test import override_settings
 
+from djxi.apps import DjxiAppConfig
 from djxi.conf import package_settings as djxi_settings
 
 
@@ -17,3 +21,18 @@ def test_conf_DX_SECTION_TAG():
         section_name = getattr(djxi_settings, "DX_SECTION_TAG", None)
         assert section_name is not None
         assert section_name == "my-section-tag"
+
+
+def test_app_ready_checks():
+    mod = types.ModuleType("djxi")
+    mod.__file__ = __file__
+    app = DjxiAppConfig("djxi", mod)
+    with pytest.raises(ImproperlyConfigured):
+        with override_settings(DX_HTMX_VERSION="99"):
+            app.ready()
+        with override_settings(DX_SECTION_TAG=""):
+            app.ready()
+        with override_settings(DX_SECTION_TAG=None):
+            app.ready()
+        with override_settings(DX_SECTION_TAG=99):
+            app.ready()
