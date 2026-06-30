1e2b6cc41b67aeb4dcc081a9769330a376786ed8
Philipp Rollinger <philipp.rollinger@gmail.com>
2026-06-30 17:51:15 +0700

    add test for routing shortcuts

---
+++ src/tests/test_routing_shortcuts.py
@@ -0,0 +1,89 @@
+from django.test import override_settings
+from django.urls import path, include, resolve
+
+from djxi.endpoint import DXEndpointBattery
+from djxi.router import (
+    dx_get,
+    dx_post,
+    dx_put,
+    dx_patch,
+    dx_delete,
+    dx_options,
+    dx_head,
+)
+
+SUPPORTED_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"]
+
+
+class InlineActionRouter(DXEndpointBattery):
+    section_inline = """<dx-section name="section_01">Content 1</dx-section>"""
+
+    @dx_get("act/get/", name="get-stuff")
+    def action_get(self, request):
+        return self.render_section(request, "section_01")
+
+    @dx_post("act/post/", name="post-stuff")
+    def action_post(self, request):
+        return self.render_section(request, "section_01")
+
+    @dx_put("act/put/", name="put-stuff")
+    def action_put(self, request):
+        return self.render_section(request, "section_01")
+
+    @dx_patch("act/patch/", name="patch-stuff")
+    def action_patch(self, request):
+        return self.render_section(request, "section_01")
+
+    @dx_delete("act/delete/", name="delete-stuff")
+    def action_delete(self, request):
+        return self.render_section(request, "section_01")
+
+    @dx_options("act/options/", name="option-stuff")
+    def action_option(self, request):
+        return self.render_section(request, "section_01")
+
+    @dx_head("act/head/", name="head-stuff")
+    def action_head(self, request):
+        return self.render_section(request, "section_01")
+
+
+urlpatterns = [
+    path("", include((InlineActionRouter.url_patterns(), "djxi"), namespace="djxi"))
+]
+
+
+@override_settings(ROOT_URLCONF=__name__)
+def test_path_resolver():
+    true_methods = {
+        "/act/get/": {"view": "action_get", "method": ["GET"], "content": "Content 1"},
+        "/act/post/": {
+            "view": "action_post",
+            "method": ["POST"],
+            "content": "Content 1",
+        },
+        "/act/put/": {"view": "action_put", "method": ["PUT"], "content": "Content 1"},
+        "/act/patch/": {
+            "view": "action_patch",
+            "method": ["PATCH"],
+            "content": "Content 1",
+        },
+        "/act/delete/": {
+            "view": "action_delete",
+            "method": ["DELETE"],
+            "content": "Content 1",
+        },
+        "/act/options/": {
+            "view": "action_option",
+            "method": ["OPTIONS"],
+            "content": "Content 1",
+        },
+        "/act/head/": {
+            "view": "action_head",
+            "method": ["HEAD"],
+            "content": "Content 1",
+        },
+    }
+    for request_path, truth in true_methods.items():
+        match = resolve(request_path)
+        assert match.func.__name__ == truth["view"]
+        assert match.func._routes[0][1] == truth["method"]
