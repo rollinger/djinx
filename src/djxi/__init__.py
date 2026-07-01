39ef43ed28d76ee46b929637c0ed2e34d796396b
Philipp Rollinger <philipp.rollinger@gmail.com>
2026-07-01 09:45:28 +0700

    fix export

--- src/djxi/__init__.py
+++ src/djxi/__init__.py
@@ -1,4 +1,4 @@
-from endpoint import DXEndpointBattery
-from router import dx_action
+from .endpoint import DXEndpointBattery
+from .router import dx_action
 
 __all__ = ["DXEndpointBattery", "dx_action"]
